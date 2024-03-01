5ae3aa916da121484c169b7ecd9878a4e2d533aa
devops-hemant <105566718+devops-hemant@users.noreply.github.com>
2024-03-02 01:29:06 +0530

    Update code.py

--- code.py
+++ code.py
@@ -5,12 +5,11 @@ def login():
     password = request.values.get("password")
 
     # Prepare database connection
-db = pymysql.connect("localhost")
-      cursor = db.cursor()
+    db = pymysql.connect("localhost")
+    cursor = db.cursor()
 
     # Execute the vulnerable SQL query concatenating user-provided input.
-    cursor.execute
-(
+    cursor.execute(
         "SELECT * FROM users WHERE username = '%s' AND password = '%s'"
         % (username, password)
     )
