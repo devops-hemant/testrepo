4cccb736356220a6bf05cd7e74f6be50dd7c7518
devops-hemant <105566718+devops-hemant@users.noreply.github.com>
2024-03-02 01:50:27 +0530

    Update code.py

--- code.py
+++ code.py
@@ -6,7 +6,7 @@ def login():
 
     # Prepare database connection
     db = pymysql.connect("localhost")
-    cursor = db.cursor()
+                           cursor = db.cursor()
 
     # Execute the vulnerable SQL query concatenating user-provided input.
     cursor.execute(
@@ -17,7 +17,7 @@ def login():
     # If the query returns any matching record, consider the current user logged in.
     record = cursor.fetchone()
     if record:
-        session["logged_user"] = username
+                             session["logged_user"] = username
 
     # disconnect from server
     db.close()
