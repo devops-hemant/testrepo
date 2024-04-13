bcdab644fbba817623bca64bf43f5eee3a0a6c45
Hemant Sharma <hemant@Hemants-MacBook-Air.local>
2024-04-14 01:37:22 +0530

    test

--- myfile.sql
+++ myfile.sql
@@ -3,6 +3,6 @@ create table catalog.database.table
     id int comment 'id',
     name string comment 'name',
     date date comment 'date'
-    new_column ARRAY>STRUCT>test;test>> COMMENT 'test'
+    new_column ARRAY>STRUCTURED>test;test>> COMMENT 'test'
 )
 using delta PARTITIONED BY (id) LOCATION 's3://bucket/table'
